a5ed1908363e84a50848fd35d627b9a1391f1823
ConanYu <c.zing@outlook.com>
2021-05-12 20:49:11 +0800

    fix: 减少生成HTML代码量

--- friend_rating_server/crawler/codeforces/get_user_rating.py
+++ friend_rating_server/crawler/codeforces/get_user_rating.py
@@ -8,14 +8,18 @@ def get_user_rating(handle: str) -> dict:
     try:
         rsp = requests.get(f"https://codeforces.com/api/user.rating?handle={handle}")
         result = json.loads(rsp.text)
+        result["data"] = []
         for contest in result["result"]:
-            contest["rating"] = contest["newRating"]
-            contest["timestamp"] = contest["ratingUpdateTimeSeconds"]
-            contest["url"] = "https://codeforces.com/contest/" + str(contest["contestId"])
-            contest["name"] = contest["contestName"]
-        result["data"] = result["result"]
+            cur = dict()
+            cur["rating"] = contest["newRating"]
+            cur["timestamp"] = contest["ratingUpdateTimeSeconds"]
+            cur["url"] = "https://codeforces.com/contest/" + str(contest["contestId"])
+            cur["name"] = contest["contestName"]
+            result["data"].append(cur)
+        del result["result"]
         return result
     except Exception as e:
+        logging.exception(e)
         return {
             "status": "unknown error",
             "data": [],
